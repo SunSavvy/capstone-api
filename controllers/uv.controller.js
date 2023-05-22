c7e8cb8f70cefe7cdf9e2f0cb2abe956ff0d772a
aggagah <gagahsp4@gmail.com>
2023-05-22 19:19:22 +0700

    adjust url params

--- controllers/uv.controller.js
+++ controllers/uv.controller.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 const retrieveUV = async (req, res) => {
+    const { latitute, longitude } = req.params || req.body;
     try {
         await axios
-            .get(process.env.API_URI, {
-                headers: {
-                    "x-access-token": process.env.KEY,
-                    "Content-Type": "application/json",
-                },
-            })
+            .get(
+                `https://api.openuv.io/api/v1/uv?lat=${latitute}&lng=${longitude}}&alt=100&dt=`,
+                {
+                    headers: {
+                        "x-access-token": process.env.KEY,
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
             .then((response) => {
                 console.log(response);
                 res.status(200).json({ data: response.data });
