2533516e6ee06fc47f2c0a1ae89e4077d8c7b036
aggagah <gagahsp4@gmail.com>
2023-05-25 21:00:06 +0700

    fixing query url and custom response

--- controllers/uv.controller.js
+++ controllers/uv.controller.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const retrieveUV = async (req, res) => {
-    const { latitute, longitude } = req.params || req.body;
+    const { latitute, longitude } = req.query;
     try {
         await axios
             .get(
@@ -15,7 +15,14 @@ const retrieveUV = async (req, res) => {
             )
             .then((response) => {
                 console.log(response);
-                res.status(200).json({ data: response.data });
+                res.status(200).json({
+                    data: {
+                        uv: response.data.result.uv,
+                        uv_time: response.data.result.uv_time,
+                        uv_max: response.data.result.uv_max,
+                        uv_max_time: response.data.result.uv_max_time,
+                    },
+                });
             });
     } catch (error) {
         res.status(501).json({ message: error });
